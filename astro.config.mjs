82df27613ff27b980cf00ca0ddad0c8cabd9d737
lele394 <lele394prod@gmail.com>
2026-05-14 16:49:25 +0200

    Added mermaid + floating point notes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import rehypePrism from 'rehype-prism-plus';
+import rehypeMermaid from 'rehype-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,12 +14,16 @@ export default defineConfig({
 	base: '/',
 	integrations: [mdx(), sitemap()],
 	markdown: {
+		syntaxHighlight: {
+			type: 'shiki',
+			excludeLangs: ['mermaid', 'math'],
+		},
 		shikiConfig: {
 			langAlias: {
 				fortran: 'fortran-free-form',
 			},
 		},
 		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex, rehypePrism],
+		rehypePlugins: [rehypeKatex, rehypePrism, rehypeMermaid],
 	},
 });
